90497901a2c72a842d8b9d8602dbdb04c07c7581
VitalY <simferopol.jurart@gmail.com>
2021-12-02 12:03:12 +0300

    Commit1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,40 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService userService = new UserServiceImpl();
+        List<User> userList = new ArrayList<>();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 20);
+        System.out.println("User с именем – Ivan добавлен в базу данных");
+
+        userService.saveUser("Petr", "Petrov", (byte) 21);
+        System.out.println("User с именем – Petr добавлен в базу данных");
+
+        userService.saveUser("Vasily", "Vasilyev", (byte) 22);
+        System.out.println("User с именем – Vasily добавлен в базу данных");
+
+        userService.saveUser("Sergey", "Sergeev", (byte) 23);
+        System.out.println("User с именем – Sergey добавлен в базу данных");
+
+        userList = userService.getAllUsers();
+        for (User u : userList) {
+            System.out.println(u);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static String urlDb = "jdbc:mysql://localhost:3306/testdb";
+    public static String userName = "root";
+    public static String password = "root";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try{connection = DriverManager.getConnection(urlDb, userName, password);}
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        return connection;
+    }
 }
